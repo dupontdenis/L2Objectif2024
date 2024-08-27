6c59e03aa91024a8f10b23077ea90ee0b2d31588
DENIS DUPONT <denis.dupont@univ-evry.fr>
2024-08-27 11:44:59 +0200

    first commit

---
+++ index.mjs
@@ -0,0 +1,11 @@
+import { showContent } from "./src/content.mjs";
+
+document
+  .getElementById("paragraphForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent the form from submitting the traditional way
+    const numParagraphs = document.getElementById("numParagraphs").value;
+    showContent(parseInt(numParagraphs, 10)); // Call showContent with the input value
+  });
+
+showContent();
